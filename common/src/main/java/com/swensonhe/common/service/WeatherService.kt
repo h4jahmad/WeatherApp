81c064590e94d5fd3460c0b736de4af68f67dae4
Ahmad Reza Enshaee <enshaee.ahmadreza@gmail.com>
2023-04-20 19:15:52 +0400

    refactored forecast request.

--- common/src/main/java/com/swensonhe/common/service/WeatherService.kt
+++ common/src/main/java/com/swensonhe/common/service/WeatherService.kt
@@ -1,14 +1,15 @@
 package com.swensonhe.common.service
 
+import com.swensonhe.common.entities.ActionResult
 import com.swensonhe.common.entities.LocationWeather
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface WeatherService {
 
-    @GET("forecast.json/?key=${SearchService.API_KEY}")
+    @GET("forecast.json?key=${SearchService.API_KEY}&aqi=no&alerts=no")
     suspend fun getForecast(
         @Query("q") query: String,
         @Query("days") days: Int,
-    ): LocationWeather
+    ): ActionResult<LocationWeather>
 }
