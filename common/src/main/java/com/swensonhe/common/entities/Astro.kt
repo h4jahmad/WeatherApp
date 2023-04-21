0e12ce3f5692e8eae9d1bc12bee6b1cb329a7ba2
Ahmad Reza Enshaee <enshaee.ahmadreza@gmail.com>
2023-04-21 14:07:44 +0400

    fixed issues with the remote dtos.

--- common/src/main/java/com/swensonhe/common/entities/Astro.kt
+++ common/src/main/java/com/swensonhe/common/entities/Astro.kt
@@ -8,7 +8,7 @@ data class Astro(
     @SerializedName("moonrise") val moonrise: String,
     @SerializedName("moonset") val moonset: String,
     @SerializedName("moon_phase") val moonPhase: String,
-    @SerializedName("moon_illumination") val moonIllumination: String,
+    @SerializedName("moon_illumination") val moonIllumination: Double,
     @SerializedName("is_moon_up") val isMoonUp: Int,
     @SerializedName("is_sun_up") val isSunUp: Int,
 )
